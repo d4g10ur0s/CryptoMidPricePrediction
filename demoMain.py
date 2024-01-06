dff757bebdc4806aa725bdae46098746bd9aa790
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2024-01-06 15:15:58 +0200

    Add files via upload

--- demoMain.py
+++ demoMain.py
@@ -4,7 +4,7 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 import tensorflow as tf
 from keras.models import Sequential , load_model
 from keras.layers import LSTM, Dense, Dropout
@@ -18,18 +18,18 @@ import datasetCreation as dC
 symbol = 'XRPUSDT'
 datasetName = '\\dataset.csv'
 processedDatasetName = '\\processedDataset.csv'
-modelName = 'MoMoney.keras'
-timeSeriesLength = 8
+modelName = '\\MoMoney.keras'
+timeSeriesLength = 15
 
 def build_model(input_shape):
     # create model
     model = Sequential()
     model.add(LSTM(input_shape[0],activation='sigmoid',recurrent_activation="tanh",
                    input_shape=input_shape, return_sequences=True))
-    model.add(LSTM(input_shape[0]*input_shape[1],activation='sigmoid',recurrent_activation="tanh",return_sequences=True,))
+    model.add(LSTM(input_shape[0]*input_shape[1],activation='tanh',recurrent_activation="sigmoid",return_sequences=True,))
     model.add(LSTM(input_shape[0],activation='sigmoid',recurrent_activation="tanh"))
     model.add(Dense(input_shape[1],activation='sigmoid'))
-    model.add(Dense(2, activation='sigmoid'))
+    model.add(Dense(2, activation='linear'))
     return model
 
 def train_model(X_train, y_train, epochs, batch_size, learning_rate, momentum):
@@ -39,7 +39,7 @@ def train_model(X_train, y_train, epochs, batch_size, learning_rate, momentum):
     optimizer = SGD(learning_rate=learning_rate, momentum=momentum)
     model.compile(optimizer=optimizer, loss='mean_squared_error')
     # Define early stopping
-    early_stopping = EarlyStopping(monitor='loss', min_delta=5e-5, patience=1, verbose=1)
+    early_stopping = EarlyStopping(monitor='loss', min_delta=5e-8, patience=1, verbose=1)
     # Train the model
     model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, callbacks=[early_stopping], verbose=1)
     return model
@@ -66,16 +66,16 @@ def main():
             timeThreshold = 0
             dC.updateDataset(os.getcwd()+datasetName, os.getcwd()+processedDatasetName, symbol)
         # comute mse
-        if not (len(lastPrediction)==0) :
+        if not (len(lastPrediction)<5) :
             my_test = pd.DataFrame(data=a.loc[len(a)-1,["Mean price"]]).transpose()
-            #mse = mean_absolute_error(lastPrediction[0],my_test)
+            #mse = mean_absolute_error([lastPrediction[0].loc[0,"Mean price"]],my_test)
             mse = mean_squared_error([lastPrediction[0].loc[0,"Mean price"]],my_test)
             print('MSE : ' + str(mse))
         else:
             mse = 5e-6
             print('Cannot compute mse')
         # predict or train
-        if os.path.exists(os.getcwd()+modelName) and mse<5e-4:
+        if (os.path.exists(os.getcwd()+modelName) and mse<5e-5):
             # make prediction
             df = (a.loc[int(a.shape[0] * 4/5):, :]).reset_index(drop=True)
             df['Mean price'] = df['Mean price'] / df['Mean price'].max()
@@ -85,7 +85,7 @@ def main():
             predictions=loaded_model.predict(X_test.reshape((-1,timeSeriesLength,len(a.keys()))))
             lastPrediction.append(pd.DataFrame(data=predictions,columns=["Mean price","Trends percentage"]))
             # show current situation
-            if len(lastPrediction)==2:# last prediction is next prediction
+            if len(lastPrediction)>=6:# last prediction is next prediction
                 lastPrediction.pop(0)
             print(lastPrediction[len(lastPrediction)-1])
             currentValue = dC.getCurrentValue(symbol)
@@ -94,7 +94,7 @@ def main():
                 print('currentValue : '+str(currentValue) + ' > lastPrediction : ' + str(lastPrediction[len(lastPrediction)-1].loc[0,'Mean price']))
             else:
                 print('currentValue : '+str(currentValue) + ' < lastPrediction : ' + str(lastPrediction[len(lastPrediction)-1].loc[0,'Mean price']))
-        else:
+        elif timeThreshold==0:
             # make X and y
             df = (a.loc[int(a.shape[0] * 3/4):, :]).reset_index(drop=True)
             df_1 = df.loc[:, ['Mean price', 'Trends percentage']]
