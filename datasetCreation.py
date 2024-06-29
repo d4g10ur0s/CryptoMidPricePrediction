ef49d4572173e26a699ba64fd711b0c829a3d3ad
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2024-06-29 15:50:23 +0300

    Add files via upload

--- datasetCreation.py
+++ datasetCreation.py
@@ -10,6 +10,10 @@ import time
 import datetime as dtime
 # Data Analysis Modules
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import torch
+import matplotlib.pyplot as plt
 
 def categorize_move(trend):
     if trend < -0.001:
@@ -24,28 +28,89 @@ def categorize_move(trend):
 # preprocess dataset for coin
 def preprocessDataset(dsName ,pdsName):
     df = pd.read_csv(dsName)
-    df = df.drop(columns=["Open time","Close time",
-                          "Quote asset volume","Number of trades","Taker buy base asset volume",
-                          "Taker buy quote asset volume","Ignore"])
+    # Calculate correlation matrix
+    df = df.drop(columns=["Open time","Close time","Volume",'Taker buy base asset volume','Taker buy quote asset volume',
+                          "Quote asset volume","Number of trades","Ignore",])
     # manual features
     # mean price
     df['Mean price']=(df['High price']+df['Low price'])/2
-    df["Trends"]= df['Mean price'] - df['Mean price'].shift(-5)
-    df.loc[0:5,'Trends'] = df.loc[0:5,'Mean price']
-    # normalize values
-    df['Volume']=df['Volume']/ df['Volume'].max()
-    df['High price']=df['High price']/ df['High price'].max()
-    df['Low price']=df['Low price']/ df['Low price'].max()
-    df['Close price']=df['Close price'] / df['Close price'].max()
-    df['Open price']=df['Open price'] / df['Open price'].max()
-    df['Trends']=df['Trends']/df['Trends'].abs().max() # it's a percentage
+    # Mean averages seem to help
+    df['MA_5'] = df['Mean price'].rolling(window=5).mean()
+    df['MA_30'] = df['Mean price'].rolling(window=30).mean()
+    df['MA_60'] = df['Mean price'].rolling(window=60).mean()
+    #df['MA_9'] = df['Mean price'].rolling(window=9).mean()
+    df['Log Earnings'] = np.log(df['Mean price']) - np.log(df['Mean price'].shift(-10))
+    df['Percentage Earnings'] = (df['Close price'] - df['Open price'].shift(-10))/df['Open price'].shift(-10)
+    df['Mean Price Earnings'] = (df['Mean price'] - df['Mean price'].shift(-10))/df['Mean price'].shift(-10)
+    #df['Next price2'] = df['Mean price'].shift(-2)
+    #df['Next price3'] = df['Mean price'].shift(-3)
+    df = df.dropna()
+
+    # let's see with fourrier
+    '''
+    for lag in range(5,20):
+        # Compute autocorrelation matrix for all pairs of variables up to the specified lag
+        for j in range(10) :
+            interval_data = np.fft.fftn(df.iloc[j*lag : j*lag + lag])
+            print(str(interval_data))
+            dft_result = np.fft.fftn(interval_data)
+            frequencies = np.fft.fftfreq(len(interval_data))
+            magnitude_spectrum = np.abs(dft_result)
+            print(str(frequencies))
+            plt.figure(figsize=(10, 6))
+            plt.plot(frequencies, magnitude_spectrum)
+            plt.title(f'Magnitude Spectrum - {lag}')
+            plt.xlabel('Frequency')
+            plt.ylabel('Magnitude')
+            plt.grid(True)
+            plt.show()
+            # something else
+            print(str(np.log(np.abs(np.fft.fftshift(dft_result))**2)))
+            plt.imshow(np.log(np.abs(np.fft.fftshift(dft_result))**2))
+            plt.show()
+
+    '''
+    '''
+    correlation_matrix = df.corr()
+    # Create a heatmap
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+    plt.title('Correlation Heatmap')
+    plt.show()
+
+    # Calculate covariance matrix
+    covariance_matrix = df.cov()
+    # Create a heatmap
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(covariance_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+    plt.title('Covariance Heatmap')
+    plt.show()
+
+        # Plot autocorrelation matrix as a heatmap
+        plt.figure(figsize=(10, 6))
+        sns.heatmap(autocorr_matrix, cmap='coolwarm', annot=True, fmt=".2f", cbar=True)
+        plt.title(f'Autocorrelation Matrix at Lag {lag}')
+        plt.xlabel('Variables')
+        plt.ylabel('Variables')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
+        '''
+
+    dfKeys = df.keys()
+    for i in dfKeys :
+        if i == 'Mean price' or i == 'Trends':
+            pass
+        else:
+            df[i] = df[i]/df[i].abs().max()
 
     df = df.dropna()
+
     # Save the processed file
     df.to_csv(pdsName,index=False)
 # create dataset for coin
 def createDataset(pathName , symbol):
-    tstartTime=dtime.datetime(2021,1,1,0,0,0)
+    tstartTime=dtime.datetime(2022,1,1,0,0,0)
     client = load_binance_creds('auth/auth.yml')
     interval = Client.KLINE_INTERVAL_1MINUTE
     df = pd.DataFrame(data=[])
@@ -113,7 +178,10 @@ def updateDataset(dsName , pdsName,symbol):
         #endwhile
     df=df.drop_duplicates(subset=['Open time'])
     df.reset_index(drop=True, inplace=True)
-    df.to_csv(dsName,index=False)
+    if len(df)>1051200:
+        df[int(len(df)*0.2):].to_csv(dsName,index=False)
+    else:
+        df.to_csv(dsName,index=False)
     preprocessDataset(dsName ,pdsName)
 
 def getCurrentValue(symbol):
--- demoMain.py
+++ demoMain.py
@@ -9,8 +9,9 @@ from sklearn.metrics import mean_squared_error,mean_absolute_error
 import tensorflow as tf
 import keras as kr
 from keras.models import Sequential , load_model
-from keras.layers import LSTM, Dense, Dropout , SimpleRNN
-from keras.optimizers import SGD
+from keras.layers import LSTM, Dense, Dropout , SimpleRNN , Bidirectional , GRU, Input
+from tensorflow.keras import regularizers
+from keras.optimizers import SGD , Adagrad , RMSprop
 from keras.callbacks import EarlyStopping
 from keras import backend as K
 import matplotlib.pyplot as plt
@@ -18,6 +19,11 @@ import matplotlib.pyplot as plt
 import datasetCreation as dC
 import bTrade as bT
 import sTrade as sT
+import keras.backend as K
+
+# Define custom Softplus loss function
+def softplus_loss(y_true, y_pred):
+    return K.log(1 + K.exp(y_pred))
 
 def get_public_ip():
     response = requests.get('https://api.ipify.org')
@@ -28,16 +34,36 @@ symbol = 'XRPUSDT'
 datasetName = '\\dataset.csv'
 processedDatasetName = '\\processedDataset.csv'
 modelName = '\\MoMoney.keras'
-timeSeriesLength = 5
+timeSeriesLength = 30
+
+def huber_loss(y_true, y_pred, delta=1.0):
+    error = y_true - y_pred
+    is_small_error = tf.abs(error) <= delta
+    squared_loss = 0.5 * tf.square(error)
+    linear_loss = delta * (tf.abs(error) - 0.5 * delta)
+    return tf.where(is_small_error, squared_loss, linear_loss)
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true)))
+
+def torchModel(input_shape):
+    pass
 
 def build_model(input_shape):
+    print(str(input_shape))
     # create model
     model = Sequential()
-    model.add(LSTM(input_shape[0],activation='sigmoid',recurrent_activation="tanh",
-                   input_shape=input_shape, return_sequences=True))
-    model.add(LSTM(input_shape[0]*input_shape[1],activation='sigmoid',recurrent_activation="tanh",return_sequences=True,))
-    model.add(LSTM(input_shape[0],activation='sigmoid',recurrent_activation="tanh"))
-    model.add(Dense(2, activation='linear'))
+    model.add(Input(shape=input_shape))
+    model.add(LSTM(input_shape[0], activation='tanh',
+                   input_shape=input_shape, return_sequences=True,return_state=False,recurrent_activation='sigmoid',go_backwards=False
+                   ))
+    model.add(Bidirectional(LSTM(input_shape[1]**2, activation='tanh',
+                   input_shape=input_shape, return_sequences=True, return_state=False,recurrent_activation='sigmoid',go_backwards=False
+                   )))
+    model.add(LSTM(input_shape[0], activation='tanh',
+                   input_shape=input_shape, return_sequences=False,return_state=False,recurrent_activation='sigmoid',go_backwards=False
+                   ))
+    model.add(Dense(1, activation='linear'))
     return model
 
 def train_model(X_train, y_train, X_val, y_val, epochs, batch_size, learning_rate, momentum):
@@ -45,15 +71,17 @@ def train_model(X_train, y_train, X_val, y_val, epochs, batch_size, learning_rat
     model = build_model(input_shape)
     # Compile the model
     optimizer = SGD(learning_rate=learning_rate, momentum=momentum)
+    #optimizer = RMSprop(learning_rate=learning_rate)
+    #model.compile(optimizer=optimizer, loss=root_mean_squared_error)
     model.compile(optimizer=optimizer, loss='mean_squared_error')
     # Define early stopping
-    early_stopping = EarlyStopping(monitor='loss', min_delta=5e-4, patience=2, verbose=1)
+    early_stopping = EarlyStopping(monitor='loss', min_delta=1e-5, patience=2, verbose=1)
     # Train the model
     history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
                         validation_data=(X_val, y_val), callbacks=[early_stopping], verbose=1)
     return model , history
 
-def main():
+def demoMain():
     global datasetName, processedDatasetName, modelName, timeSeriesLength
     if not os.path.exists(os.getcwd()+datasetName):
         # create dataset
@@ -67,12 +95,18 @@ def main():
     timeThreshold = 0
     lastPrediction = []
     mse = 0
+    newData = True
+    dumb = pd.read_csv(os.getcwd()+datasetName)
+    maxMean = 1#((dumb['High price']+dumb['Low price'])/2).max()
+    mpred = None
+
     while 1 :
         # Load processed dataset
-        a = pd.read_csv(os.getcwd()+processedDatasetName)
         timeThreshold+=abs(lastTimestamp - dt.datetime.timestamp(dt.datetime.now()))
         if timeThreshold>60:
+            newData = True
             timeThreshold = 0
+            lastTimestamp = dt.datetime.timestamp(dt.datetime.now())
             dC.updateDataset(os.getcwd()+datasetName, os.getcwd()+processedDatasetName, symbol)
         # comute mse
         try :
@@ -80,71 +114,113 @@ def main():
         except :
             pass
 
-        if not (len(lastPrediction)<5) :
-            my_test = pd.DataFrame(data=a.loc[len(a)-1,["Mean price"]]).transpose()
-            #mse = mean_absolute_error([lastPrediction[0].loc[0,"Mean price"]],my_test)
-            mse = mean_squared_error([lastPrediction[0].loc[0,"Mean price"]],my_test)
+        a = pd.read_csv(os.getcwd()+processedDatasetName)
+        if not (len(lastPrediction)<10) and newData :
+            newData = False
+            my_test = pd.DataFrame(data=a.loc[len(a)-10,["Mean price"]]*maxMean).transpose()
+            # predictions variable acts like a queue
+            mse = mean_absolute_error([lastPrediction[0].loc[0,"Mean price"]*maxMean],my_test)
+            #mse = mean_squared_error([lastPrediction[0].loc[0,"Mean price"]],my_test)
             print('MSE : ' + str(mse))
         else:
             mse = 5e-6
             print('Cannot compute mse')
         # predict or train
-        if (os.path.exists(os.getcwd()+modelName) and mse<5e-5):
+        if (os.path.exists(os.getcwd()+modelName) and mse<40e-4):
             # make prediction
-            df = (a.loc[int(a.shape[0] * 0.95):, :]).reset_index(drop=True)
-            X_test = np.array(df.iloc[len(df)-timeSeriesLength:], dtype=np.float32)
-            loaded_model = load_model(os.getcwd()+modelName)
-            # prediction
-            predictions=loaded_model.predict(X_test.reshape((-1,timeSeriesLength,len(a.keys()))))
-            lastPrediction.append(pd.DataFrame(data=predictions,columns=["Mean price","Trends percentage"]))
+            if newData :
+                df = (a.loc[int(a.shape[0] * 0.95):, :]).reset_index(drop=True)
+                X_test = np.array(df.iloc[len(df)-timeSeriesLength:], dtype=np.float32)
+                X_test = X_test.reshape((-1,timeSeriesLength,len(a.keys())))
+                loaded_model = load_model(os.getcwd()+modelName)
+                # prediction
+                predictions=loaded_model.predict(np.log(np.abs(np.fft.fftshift(np.fft.fftn(X_test)))**2))
+                lastPrediction.append(pd.DataFrame(data=predictions,columns=["Mean price"]))
+                mpred = predictions[0]
+
             # show current situation
-            if len(lastPrediction)>=6:# last prediction is next prediction
+            if len(lastPrediction)>=11:# last prediction is next prediction
                 lastPrediction.pop(0)
-            print(lastPrediction[len(lastPrediction)-1])
+            print('Mean price : ' + str(lastPrediction[len(lastPrediction)-1].loc[0, 'Mean price']*maxMean))
+            #print('Trends : ' + str(round(lastPrediction[len(lastPrediction)-1].loc[0 , 'Trends'] , 3))+'%')
             currentValue = dC.getCurrentValue(symbol)
-            mpred = predictions[0]
-            if float(currentValue) > mpred[0]  :
-                print('currentValue : '+str(currentValue) + ' > lastPrediction : ' + str(lastPrediction[len(lastPrediction)-1].loc[0,'Mean price']))
-                #for i in range(5):
-                try :
-                    sT.sTrade(mpred[0]+mpred[0]*mpred[1])
-                except :
-                    print("Could not make the order")
-            else:
-                print('currentValue : '+str(currentValue) + ' < lastPrediction : ' + str(lastPrediction[len(lastPrediction)-1].loc[0,'Mean price']))
-                for i in range(2):
+            if float(currentValue)>maxMean:
+                maxMean=float(currentValue)
+            print("Current Difference : " + str(abs(float(currentValue)-mpred[0]*maxMean)))
+            if float(currentValue) > mpred[0] *maxMean :
+                print('currentValue : '+str(currentValue) + ' > lastPrediction : ' + str(mpred[0] *maxMean))
+                for i in range(3):
                     try :
-                        bT.bTrade(mpred[0]-mpred[0]*mpred[1])
+                        if (abs(float(currentValue)-mpred[0]*maxMean)>4.5e-3):
+                            bT.bTrade(float(currentValue) - .5e-3)
+                        elif(abs(float(currentValue)-mpred[0]*maxMean)<=4e-3 and abs(float(currentValue)-mpred[0]*maxMean)>=1.5e-3):
+                            sT.sTrade(float(currentValue))
                     except :
                         print("Could not make the order")
+            else:
+                print('currentValue : '+str(currentValue) + ' < lastPrediction : ' + str(mpred[0] *maxMean))
+                for i in range(3):
+                    try :
+                        if(abs(float(currentValue)-mpred[0]*maxMean)>3e-3):
+                            sT.sTrade(float(currentValue) + .5e-3)
+                        elif(abs(float(currentValue)-mpred[0]*maxMean)>1e-3):
+                            bT.bTrade(float(currentValue))
+                    except :
+                        print("Could not make the order")
+            # get time again
+            #lastTimestamp = dt.datetime.timestamp(dt.datetime.now())
         elif timeThreshold==0:
+            # get time again
+            #lastTimestamp = dt.datetime.timestamp(dt.datetime.now())
+            lastPrediction = []
+            mse = 5e-6
             # make X and y
-            df = (a.loc[int(a.shape[0] * 0.75):, :]).reset_index(drop=True)
-            df_1 = df.loc[:, ['Mean price', 'Trends']]
+            df = (a.loc[int(a.shape[0] * 0.87):, :]).reset_index(drop=True)
+            print(str(df.keys()))
+            df_1 = df.loc[:, ['Mean price']]
             print(np.isnan(df_1).any())
             X, y = [], []
             for i in range(df.shape[0] - timeSeriesLength):
                 try:
-                    y.append(df_1.iloc[i+timeSeriesLength+5])
-                    X.append(df.iloc[i:i+timeSeriesLength])
+                    y.append(df_1.iloc[i+timeSeriesLength+10])
+                    # how about taking the dft ?
+                    X.append(np.log(np.abs(np.fft.fftshift(np.fft.fftn(df.iloc[i:i+timeSeriesLength])))**2))
                 except:
                     print("Going for training.")
             # Training dataset in numpy format
             X = np.array(X, dtype=np.float32)
             y = np.array(y, dtype=np.float32)
             # Split the data into training and validation sets
-            X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15, random_state=8)
+            X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.05, random_state=8)
             print("NaN values in X_train:", np.isnan(X_train).any())
             print("NaN values in y_train:", np.isnan(y_train).any())
             # Set hyperparameters
-            epochs = 25
-            batch_size = int(timeSeriesLength/2)
+            epochs = 20
+            batch_size = int(timeSeriesLength/5)
+            learning_rate = 0.025
+            momentum = 0.785
+            '''
             learning_rate = float(input('Set learning rate: '))
             momentum = float(input('Set momentum: '))
+            '''
             # Train the model
             trained_model , history = train_model(X_train, y_train, X_val, y_val, epochs, batch_size, learning_rate, momentum)
             # Save the model
             trained_model.save(os.getcwd()+modelName)
+            # Plot the signals
+            y_pred = trained_model.predict(X_val)
+            print("MSE : " + str(np.mean( (y_val - y_pred)**2 )) )
+            # Plot real points as circles
+            plt.plot(range(0,200), y_val[:200, 0], marker='o', label='Real Points')  # Circles for real points
+            # Plot predicted points as crosses (x)
+            plt.plot( range(0,200), y_pred[:200, 0], marker='x', label='Predicted Points')  # Crosses for predicted points
+            plt.title('Real Points vs Predicted Points')
+            plt.xlabel('Dimension 1')
+            plt.ylabel('Dimension 2')
+            plt.legend()
+            plt.savefig(str(dt.datetime.timestamp(dt.datetime.now()))+".png")
+            plt.clf()  # Clear the current figure
+            '''
             # Plot training loss
             plt.plot(history.history['loss'], label='Training Loss')
             plt.plot(history.history['val_loss'], label='Validation Loss')
@@ -153,9 +229,15 @@ def main():
             plt.ylabel('Loss')
             plt.legend()
             plt.show()
-        # get time again
-        lastTimestamp = dt.datetime.timestamp(dt.datetime.now())
-        time.sleep(10)
+            '''
+        time.sleep(2)
 
+def main():
+    while 1 :
+        try :
+            demoMain()
+        except :
+            print("Error occured")
+        time.sleep(60 * 2)
 if __name__ == '__main__':
     main()
